37524c1f9dccdeef6e84830747e31ce86799fa12
Ayesh Rodrigo <ayeshrodz@gmail.com>
2022-06-11 11:50:51 +0530

    Modify controller for async reqs for DB conn

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,35 +1,38 @@
+const asyncHandler = require('express-async-handler')
+
+
 // @desc    Get projects
 // @route   GET /api/projects
 // @access  Private
-const getProjects = (req, res) => {
+const getProjects = asyncHandler(async (req, res) => {
     res.json({message: 'Get projects'})
-}
+})
 
 // @desc    Set project
 // @route   POST /api/projects
 // @access  Private
-const createProject = (req, res) => {
+const createProject = asyncHandler(async (req, res) => {
     if(!req.body.name) {
         res.status(400)
         throw new Error('Please add a name for the project')
     }
 
     res.status(200).json({message: 'Set projects'})
-}
+})
 
 // @desc    Update a project
 // @route   PUT /api/projects/:id
 // @access  Private
-const updateProject = (req, res) => {
+const updateProject = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update project ${req.params.id}`})
-}
+})
 
 // @desc    Delete a project
 // @route   DELETE /api/projects/:id
 // @access  Private
-const deleteProject = (req, res) => {
+const deleteProject = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete project ${req.params.id}`})
-}
+})
 
 
 
