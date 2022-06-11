b3fe736771a0feb947cdf21fb26f2ec95b8eac86
Ayesh Rodrigo <ayeshrodz@gmail.com>
2022-06-11 11:35:45 +0530

    Add error handler middleware

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -2,28 +2,33 @@
 // @route   GET /api/projects
 // @access  Private
 const getProjects = (req, res) => {
-    res.status(200).json({message: 'Get goals'})
+    res.json({message: 'Get projects'})
 }
 
 // @desc    Set project
 // @route   POST /api/projects
 // @access  Private
 const createProject = (req, res) => {
-    res.status(200).json({message: 'Set goals'})
+    if(!req.body.name) {
+        res.status(400)
+        throw new Error('Please add a name for the project')
+    }
+
+    res.status(200).json({message: 'Set projects'})
 }
 
 // @desc    Update a project
 // @route   PUT /api/projects/:id
 // @access  Private
 const updateProject = (req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`})
+    res.status(200).json({message: `Update project ${req.params.id}`})
 }
 
 // @desc    Delete a project
 // @route   DELETE /api/projects/:id
 // @access  Private
 const deleteProject = (req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
+    res.status(200).json({message: `Delete project ${req.params.id}`})
 }
 
 
